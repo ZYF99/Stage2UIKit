c0ec45de69e67f7c4c7395612923b887949aec88
zhangyf <yunfeng.zhang@fenrir-inc.com.cn>
2019-07-16 18:47:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/stage2_uikit/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.stage2_uikit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
